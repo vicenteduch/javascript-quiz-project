5e0fd24e3abae96c83e387489174a8b0254aca64
Vicente Duch <vhduch@gmail.com>
2024-10-07 17:01:29 +0200

    day 1 completed

--- src/question.js
+++ src/question.js
@@ -1,7 +1,25 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
 
-    // 2. shuffleChoices()
-}
+    shuffleChoices() {
+        
+            const shuffle = [];
+            for(let i = this.choices.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i+1));
+                const temp = this.choices[i];
+                this.choices[i] = this.choices[j];
+                this.choices[j] = temp;      
+            }
+            console.log(shuffle);
+            return shuffle;
+        }
+    }
+
+  
+
+
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,49 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
+     
     // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
     
-    // 3. moveToNextQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
 
-    // 4. shuffleQuestions()
+    }
+    
+    moveToNextQuestion() {
+        return this.currentQuestionIndex++
+    }
 
-    // 5. checkAnswer(answer)
+    shuffleQuestions() {
+        const shuffle = [];
+        for(let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i+1));
+            const temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp;      
+        }
+        console.log(shuffle);
+        return shuffle;
+    }
+    
+    checkAnswer(answer) {
+        
+        if (this.questions[this.currentQuestionIndex].answer === answer) {
+            this.correctAnswers++;
+        } 
+        return this.correctAnswers;
+    }
 
-    // 6. hasEnded()
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false
+        } else if (this.currentQuestionIndex === this.questions.length){
+            return true
+        }
+    }
 }
